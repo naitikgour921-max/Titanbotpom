fe4ebe3012004f8a05c512253cbe3345effa3e17
naitikgour921-max <naitikgour921@gmail.com>
2026-06-12 21:01:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const { MongoClient } = require('mongodb');
 
-const botToken = "8939249559:AAFSrQ_X0pGCP6fwtKgv9K1S8dtavKOOYV0";
+const botToken = "8459603547:AAE8kVsK7SxDBxepYVtiD4lp4W_BfQre_2k";
 const adminId = "5291409360";
 const mongoUrl = process.env.MONGO_URL; 
 
